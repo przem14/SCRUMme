4dec78555db60342c14291f5ded50c20a3fbd540
przem14 <przem.wawrzyniak@gmail.com>
2014-12-24 15:56:04 +0100

    Added file for user_flows integration tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
+  end
+
+  test 'index should render correct template and layout' do
+    get :index
+    assert_template :index
+    assert_template layout: 'layouts/application'
   end
 
 end
